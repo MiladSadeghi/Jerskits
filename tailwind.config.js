922243e02b9cf9cc36be7e834058571d92298a90
MiladSadeghi <miladsadeghi2323@gmail.com>
2023-06-27 15:57:27 +0330

    Initial setup of the application. Installed necessary packages and made basic configurations.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
