0dc0b4a411e9b4ae69ac49adc0d19e812dc2ec59
Milad Sadeghi <miladsadeghi2323@gmail.com>
2023-10-01 12:26:41 +0330

    feat(server/landing): Add Kids Collection products to the landing page route
    - Introduce a new endpoint to retrieve Kids Collection products based on their brand.

--- server/src/api/controller/landing.controller.js
+++ server/src/api/controller/landing.controller.js
@@ -1,6 +1,7 @@
 import ProductModel from "../models/product.model.js";
 
-const landingProvider = async (req, res, next) => {
+export const landingProvider = async (req, res, next) => {
+  const { isUser } = req;
   try {
     const products = await ProductModel.find();
     const headerProductsId = [
@@ -8,55 +9,89 @@ const landingProvider = async (req, res, next) => {
       "64e65e04b7c149604dc3c79a",
       "64f988cf965a3409ef664c90",
     ];
-    const headerProducts = products.filter((product) =>
-      headerProductsId.includes(product._id.toString())
-    );
+    const headerProducts = products
+      .filter((product) => headerProductsId.includes(product._id.toString()))
+      .map((product) => product.toObject());
 
-    const plainHeaderProducts = headerProducts.map((product) =>
-      product.toObject()
-    );
-
-    plainHeaderProducts[0].posterTitle = "HOME KIT 23/24";
-    plainHeaderProducts[0].stadiumImage =
+    headerProducts[0].posterTitle = "HOME KIT 23/24";
+    headerProducts[0].stadiumImage =
       "https://s13.uupload.ir/files/miladsdgh/inside-anfield-promo-10042023_6diy.webp";
-    plainHeaderProducts[0].teamLogo =
+    headerProducts[0].teamLogo =
       "https://s6.uupload.ir/files/liverpool_fc.svg_jt8c.png";
-    plainHeaderProducts[0].teamName = "liverpool";
-    plainHeaderProducts[0].howWasMade =
+    headerProducts[0].teamName = "liverpool";
+    headerProducts[0].howWasMade =
       "The recycled polyester used in Nike products begins as recycled plastic bottles, which are cleaned, shredded into flakes and converted into pellets.";
-    plainHeaderProducts[0].posterDescription =
+    headerProducts[0].posterDescription =
       "In addition to reducing waste, recycled polyester produces up to 30% lower carbon emissions than virgin polyester. Nike diverts an average of 1 billion plastic bottles annually from landfill and waterways.";
-    plainHeaderProducts[0].kitLogo = plainHeaderProducts[0].gallery[3];
+    headerProducts[0].kitLogo = headerProducts[0].gallery[3];
 
-    plainHeaderProducts[1].posterTitle = "23/24 HOME AUTHENTIC JERSEY";
-    plainHeaderProducts[1].stadiumImage =
+    headerProducts[1].posterTitle = "23/24 HOME AUTHENTIC JERSEY";
+    headerProducts[1].stadiumImage =
       "https://s13.uupload.ir/files/miladsdgh/gettyimages-1488651478_u6xx.jpg";
-    plainHeaderProducts[1].teamLogo =
+    headerProducts[1].teamLogo =
       "https://s6.uupload.ir/files/real-madrid-c-f-logo-c08f61d801-seeklogo.com_ybry.png";
-    plainHeaderProducts[1].teamName = "Real Madrid";
-    plainHeaderProducts[1].howWasMade =
+    headerProducts[1].teamName = "Real Madrid";
+    headerProducts[1].howWasMade =
       "Tradition gets a golden touch in the Real Madrid 23/24 Home Authentic Jersey. Keeping a crisp all-white base, the striking kit continues the thread of powerful design, delivering a sleek, lightweight jersey steeped in legendary soccer history.";
-    plainHeaderProducts[1].posterDescription =
+    headerProducts[1].posterDescription =
       "With a nod to older kits, gold accents from previous kits are re-introduced alongside navy framing. An allover, sublimation print is a subtle update to the classic all-white color, complemented by a new soccer silhouette that lets you take the clubs elegance and style off-pitch.";
-    plainHeaderProducts[1].kitLogo = plainHeaderProducts[1].gallery[3];
+    headerProducts[1].kitLogo = headerProducts[1].gallery[3];
 
-    plainHeaderProducts[2].posterTitle = "F.C. Barcelona 2023/24 Stadium Home";
-    plainHeaderProducts[2].stadiumImage =
+    headerProducts[2].posterTitle = "F.C. Barcelona 2023/24 Stadium Home";
+    headerProducts[2].stadiumImage =
       "https://s13.uupload.ir/files/miladsdgh/fbl-esp-liga-barcelona-villarreal-2048x1366_bm84.webp";
-    plainHeaderProducts[2].teamLogo =
+    headerProducts[2].teamLogo =
       "https://s6.uupload.ir/files/kisspng-fc-barcelona-logo-vector-graphics-football-image-5c71e47872d597.9276381515509679284704_b8rh.png";
-    plainHeaderProducts[2].teamName = "Barcelona";
-    plainHeaderProducts[2].howWasMade =
+    headerProducts[2].teamName = "Barcelona";
+    headerProducts[2].howWasMade =
       "The F.C. Barcelona 2023/24 Home kit honours the 30-year journey of the Women's squad being incorporated into the club. The crest has a hidden diamond shape that was historically used by the Women's team, and the classic stripes have a diamond pattern along the edges. Our Stadium collection pairs replica design details with sweat-wicking technology to give you a game-ready look inspired by your favorite team.";
-    plainHeaderProducts[2].posterDescription =
+    headerProducts[2].posterDescription =
       "The recycled polyester used in Nike products begins as recycled plastic bottles, which are cleaned, shredded into flakes and converted into pellets. From there, the pellets are spun into new, high-quality yarn used in our products, delivering peak performance with a lower impact on the environment.";
-    plainHeaderProducts[2].kitLogo = plainHeaderProducts[2].gallery[5];
+    headerProducts[2].kitLogo = headerProducts[2].gallery[5];
 
-    return res.status(200).json({ error: false, header: plainHeaderProducts });
+    const kidsCollectionProducts = products.filter(
+      (product) => product.gender === "kid" && product.brand === "nike"
+    );
+
+    return res
+      .status(200)
+      .json({ error: false, header: headerProducts, kidsCollectionProducts });
   } catch (error) {
     console.error(error);
     next(error);
   }
 };
 
-export default landingProvider;
+export const kidCollectionProvider = async (request, response, next) => {
+  const { user, isUser } = request;
+  const brand = request.params.brand || "nike";
+
+  try {
+    const validBrands = ["nike", "adidas", "jordan", "puma"];
+    if (!validBrands.includes(brand)) {
+      const errorMessage = `${brand} does not have any kid jerseys!`;
+      const error = new Error(errorMessage);
+      error.status = 404;
+      throw error;
+    }
+
+    const kidsCollectionProducts = await ProductModel.find({
+      gender: "kid",
+      brand,
+    });
+
+    if (kidsCollectionProducts.length === 0) {
+      const errorMessage = `${brand} does not have any kid jerseys!`;
+      const error = new Error(errorMessage);
+      error.status = 404;
+      throw error;
+    }
+
+    return response
+      .status(200)
+      .json({ error: false, kidCollection: kidsCollectionProducts });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -3,7 +3,11 @@ import authRouter from "./auth.route.js";
 import profileRouter from "./profile.route.js";
 import locationRouter from "./location.routes.js";
 import userRouter from "./user.route.js";
-import landingProvider from "../controller/landing.controller.js";
+import {
+  kidCollectionProvider,
+  landingProvider,
+} from "../controller/landing.controller.js";
+import { isUserSignIn } from "../middleware/verifyJWT.js";
 
 const mainRoutes = Router();
 
@@ -11,6 +15,7 @@ mainRoutes.use("/auth", authRouter);
 mainRoutes.use("/profile", profileRouter);
 mainRoutes.use("/location", locationRouter);
 mainRoutes.use("/user", userRouter);
-mainRoutes.get("/", landingProvider);
+mainRoutes.get("/", isUserSignIn, landingProvider);
+mainRoutes.get("/kid-collection/:brand?", isUserSignIn, kidCollectionProvider);
 
 export default mainRoutes;
