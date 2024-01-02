1a5731f783ad6ed13be70951bac243b8f5a6620d
Milad Sadeghi <miladsadeghi2323@gmail.com>
2024-01-03 01:49:23 +0330

    feat(client/profile layout): Add style to active route

--- client/src/components/ProfileLink/ProfileLink.tsx
+++ client/src/components/ProfileLink/ProfileLink.tsx
@@ -1,20 +1,32 @@
 import { TProfileLink } from '../../utils/profile-links'
 import { ArrowDown } from '../../icons'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { cn } from '../../utils/utils'
 
 type Props = TProfileLink & { key?: number }
 
 function ProfileLink({ link, title }: Props): JSX.Element {
   return (
-    <Link
-      className='flex justify-between ease-in-out duration-100 aria-[current=page]:p-5 aria-[current=page]:bg-neutral-light-grey'
+    <NavLink
+      className={
+        'flex justify-between ease-in-out duration-100 aria-[current=page]:p-5 aria-[current=page]:bg-neutral-light-grey'
+      }
       to={link}
     >
-      <h2 className='font-bold text-lg leading-[150%] text-primary-black'>
-        {title}
-      </h2>
-      <ArrowDown className='-rotate-90 opacity-30' width={22} height={22} />
-    </Link>
+      {({ isActive }) => (
+        <>
+          <h2
+            className={cn(
+              'font-bold text-lg leading-[150%] text-primary-black',
+              { 'bg-neutral-light-grey px-5': isActive }
+            )}
+          >
+            {title}
+          </h2>
+          <ArrowDown className='-rotate-90 opacity-30' width={22} height={22} />
+        </>
+      )}
+    </NavLink>
   )
 }
 
